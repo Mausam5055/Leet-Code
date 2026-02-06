b576f3cd75e6277efbcaabe7df2202f62eeec551
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-02-06 11:31:03 +0530

    Time: 62 ms (36.63%) | Memory: 49.4 MB (50.40%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -5,31 +5,31 @@ class Solution {
         Set<Character> vowelsSet = new HashSet<>(
             Arrays.asList('a','e','i','o','u','A','E','I','O','U')
         );
-        
+
         List<Character> vowels = new ArrayList<>();
-        
+
         // Step 1: Collect vowels
         for (char c : s.toCharArray()) {
             if (vowelsSet.contains(c)) {
                 vowels.add(c);
             }
         }
-        
+
         // Step 2: Sort vowels by ASCII
         Collections.sort(vowels);
-        
+
         // Step 3: Rebuild string
         StringBuilder result = new StringBuilder();
-        int index = 0; // pointer for sorted vowels
-        
+        int idx = 0;
+
         for (char c : s.toCharArray()) {
             if (vowelsSet.contains(c)) {
-                result.append(vowels.get(index++));
+                result.append(vowels.get(idx++));
             } else {
                 result.append(c);
             }
         }
-        
+
         return result.toString();
     }
 }
