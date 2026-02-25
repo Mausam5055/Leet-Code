beb6faa3b0fe4eff82fc7db0032695d327eeccda
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-02-25 14:06:10 +0530

    Time: 5 ms (54.41%) | Memory: 42.9 MB (81.61%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -1,29 +1,23 @@
 class Solution {
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
-        // dp[i][j] will store the minimum score to triangulate the polygon from vertex i to j
         int[][] dp = new int[n][n];
 
-        // We check shapes of increasing lengths. 
-        // len is the distance between the start vertex i and end vertex j.
-        // It starts at 2 because you need at least 3 points (distance of 2) to form a triangle.
+        // len = distance between i and j
         for (int len = 2; len < n; len++) {
             for (int i = 0; i + len < n; i++) {
                 int j = i + len;
                 dp[i][j] = Integer.MAX_VALUE;
-                
-                // Try every possible middle vertex k to form a triangle (i, k, j)
+
+                // try all possible k between i and j
                 for (int k = i + 1; k < j; k++) {
-                    // Score = (triangle i,k,j) + (left sub-polygon) + (right sub-polygon)
-                    int currentScore = values[i] * values[k] * values[j] + dp[i][k] + dp[k][j];
-                    
-                    // Keep the smallest score we find
-                    dp[i][j] = Math.min(dp[i][j], currentScore);
+                    int cost = dp[i][k] + dp[k][j] 
+                             + values[i] * values[k] * values[j];
+                    dp[i][j] = Math.min(dp[i][j], cost);
                 }
             }
         }
-        
-        // The answer is the min score to triangulate from the first to the last vertex
+
         return dp[0][n - 1];
     }
 }
