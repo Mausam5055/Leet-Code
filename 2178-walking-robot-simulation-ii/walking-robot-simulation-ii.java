8453f8eb5c3e728c68945e32647a214b9cd4c7f7
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-04-07 13:25:20 +0530

    Time: 57 ms (99.30%) | Memory: 57.6 MB (83.92%) - LeetSync

---
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -0,0 +1,58 @@
+class Robot {
+
+    int width, height;
+    int x = 0, y = 0;
+    int dir = 0; // 0=East, 1=North, 2=West, 3=South
+    
+    String[] directions = {"East", "North", "West", "South"};
+    
+    int perimeter;
+
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.perimeter = 2 * (width + height - 2);
+    }
+    
+    public void step(int num) {
+        num %= perimeter;
+        
+        // Special case: if num == 0 but we were asked to move
+        if (num == 0) num = perimeter;
+
+        while (num > 0) {
+            if (dir == 0) { // East
+                int move = Math.min(num, width - 1 - x);
+                x += move;
+                num -= move;
+                if (num > 0) dir = 1;
+            }
+            else if (dir == 1) { // North
+                int move = Math.min(num, height - 1 - y);
+                y += move;
+                num -= move;
+                if (num > 0) dir = 2;
+            }
+            else if (dir == 2) { // West
+                int move = Math.min(num, x);
+                x -= move;
+                num -= move;
+                if (num > 0) dir = 3;
+            }
+            else { // South
+                int move = Math.min(num, y);
+                y -= move;
+                num -= move;
+                if (num > 0) dir = 0;
+            }
+        }
+    }
+    
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+    
+    public String getDir() {
+        return directions[dir];
+    }
+}
