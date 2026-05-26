955b5952b3734fa2c7f91d6b724a2e4f4ee4c095
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-05-26 10:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (50.18%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -5,17 +5,14 @@ public:
 
         int n = nums.back();
 
-        // Size must be n + 1
         if (nums.size() != n + 1)
             return false;
 
-        // Check 1 to n-1
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] != i + 1)
                 return false;
         }
 
-        // Last two elements must be n
         return nums[n - 1] == n && nums[n] == n;
     }
 };
