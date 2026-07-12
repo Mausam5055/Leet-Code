254d79bc34d09c242f5c25826e7709d062b8ea8b
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-07-12 16:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.37%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -18,9 +18,8 @@ public:
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
                     ans++;
-                }
             }
         }
 
