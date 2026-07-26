fa57cccee678d3d8970d54ffa524f0524b738dec
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-07-26 00:41:19 -0700

    Time: 8 ms (81.74%) | Memory: 28.9 MB (69.37%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,41 +1,60 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
 class Solution {
 public:
-    bool can(long long T, int mountainHeight, vector<int>& workerTimes) {
-        long long removed = 0;
-
-        for (int w : workerTimes) {
-            long long k = (2LL * T) / w;
-
-            long long x = (long long)((sqrtl(1.0L + 4.0L * k) - 1.0L) / 2.0L);
-
-            while ((long long)w * x * (x + 1) / 2 > T) x--;
-            while ((long long)w * (x + 1) * (x + 2) / 2 <= T) x++;
-
-            removed += x;
-
-            if (removed >= mountainHeight)
-                return true;
-        }
-
-        return false;
-    }
-
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        long long low = 0;
-
-        long long mn = *min_element(workerTimes.begin(), workerTimes.end());
-
-        long long high = mn * 1LL * mountainHeight * (mountainHeight + 1) / 2;
-
-        while (low < high) {
-            long long mid = low + (high - low) / 2;
-
-            if (can(mid, mountainHeight, workerTimes))
-                high = mid;
-            else
-                low = mid + 1;
+        // Find the minimum worker time to calculate a reasonable upper bound
+        long long min_wt = *min_element(workerTimes.begin(), workerTimes.end());
+        long long L = 1;
+        long long mh = mountainHeight;
+        
+        // Worst-case scenario: the fastest worker reduces the entire mountain alone
+        long long R = min_wt * mh * (mh + 1) / 2;
+        long long ans = R;
+        
+        // Lambda function to check if time T is enough
+        auto check = [&](long long T) {
+            long long total_reduced = 0;
+            
+            for (int wt : workerTimes) {
+                // We need to find the max x such that x*(x+1) <= 2*T/wt
+                long long val = (2LL * T) / wt;
+                long long x = sqrt(val);
+                
+                // Adjust x to avoid precision issues with sqrt
+                while (x * (x + 1) > val) {
+                    x--;
+                }
+                while ((x + 1) * (x + 2) <= val) {
+                    x++;
+                }
+                
+                total_reduced += x;
+                
+                // If we've already met the mountain height requirement, time T is sufficient
+                if (total_reduced >= mountainHeight) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        
+        // Binary Search
+        while (L <= R) {
+            long long mid = L + (R - L) / 2;
+            
+            if (check(mid)) {
+                ans = mid;     // mid is possible, record it
+                R = mid - 1;   // Try to find a smaller sufficient time
+            } else {
+                L = mid + 1;   // mid is not enough, increase the time
+            }
         }
-
-        return low;
+        
+        return ans;
     }
 };
