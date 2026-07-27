062d76ed8f157fb9795ca2620dbe718efff659d3
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-07-27 07:19:09 -0700

    Time: 70 ms (50.00%) | Memory: 117.1 MB (47.76%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+            mp[nums[i]].push_back(i);
+
+        vector<long long> ans(n);
+
+        for (auto &it : mp) {
+            vector<int> &idx = it.second;
+
+            int m = idx.size();
+
+            vector<long long> prefix(m);
+
+            prefix[0] = idx[0];
+
+            for (int i = 1; i < m; i++)
+                prefix[i] = prefix[i - 1] + idx[i];
+
+            for (int i = 0; i < m; i++) {
+
+                long long left = 0, right = 0;
+
+                if (i > 0)
+                    left = 1LL * idx[i] * i - prefix[i - 1];
+
+                if (i < m - 1)
+                    right = (prefix[m - 1] - prefix[i]) -
+                            1LL * idx[i] * (m - i - 1);
+
+                ans[idx[i]] = left + right;
+            }
+        }
+
+        return ans;
+    }
+};
