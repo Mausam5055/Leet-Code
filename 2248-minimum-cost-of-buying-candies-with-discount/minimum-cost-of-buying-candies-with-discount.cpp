a44f07f7280ad6c56b1e238eab3d25e2575a7092
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-08-06 23:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (7.97%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,16 +1,20 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int minimumCost(vector<int>& cost) {
-        sort(cost.begin(), cost.end(), greater<int>());
-
-        int ans = 0;
-
-        for (int i = 0; i < cost.size(); i++) {
-            if ((i + 1) % 3 != 0) { // pay for first two candies
-                ans += cost[i];
+    int minimumCost(std::vector<int>& cost) {
+        // Sort costs in descending order
+        std::sort(cost.rbegin(), cost.rend());
+        
+        int total_cost = 0;
+        for (int i = 0; i < cost.size(); ++i) {
+            // Every 3rd item (0-indexed 2, 5, 8) is free
+            if (i % 3 != 2) {
+                total_cost += cost[i];
             }
         }
-
-        return ans;
+        
+        return total_cost;
     }
 };
