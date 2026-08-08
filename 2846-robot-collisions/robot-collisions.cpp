832ec144c576ec14cfa048c0c3a5ad28264e23b0
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-08-08 20:54:03 +0530

    Time: 49 ms (84.09%) | Memory: 196.6 MB (64.32%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,65 +1,67 @@
+#include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions,
-                                      vector<int>& healths,
-                                      string directions) {
-
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
         int n = positions.size();
+        
+        // Array to keep track of original indices
+        vector<int> ids(n);
+        iota(ids.begin(), ids.end(), 0); 
+        
+        // Sort indices based on starting positions from left to right
+        sort(ids.begin(), ids.end(), [&](int i, int j) {
+            return positions[i] < positions[j];
+        });
 
-        vector<array<int,4>> robots;
-
-        for(int i = 0; i < n; i++) {
-            robots.push_back({positions[i], healths[i], directions[i], i});
-        }
-
-        sort(robots.begin(), robots.end());
-
-        stack<int> st; // indices in robots array
-
-        for(int i = 0; i < n; i++) {
-
-            if(robots[i][2] == 'R') {
-                st.push(i);
-                continue;
-            }
-
-            while(!st.empty() && robots[i][1] > 0) {
-
-                int j = st.top();
-
-                if(robots[j][1] < robots[i][1]) {
-                    robots[i][1]--;
-                    robots[j][1] = 0;
-                    st.pop();
+        vector<int> st; // Stack to hold indices of processed robots
+        
+        for (int id : ids) {
+            if (directions[id] == 'R') {
+                // Moving right, no immediate collision, push to stack
+                st.push_back(id);
+            } else {
+                // Moving left, process potential collisions
+                while (!st.empty() && directions[st.back()] == 'R' && healths[id] > 0) {
+                    int top_id = st.back();
+                    
+                    if (healths[id] > healths[top_id]) {
+                        // Current 'L' robot wins
+                        healths[id] -= 1;
+                        healths[top_id] = 0; // Top robot is destroyed
+                        st.pop_back();
+                    } else if (healths[id] < healths[top_id]) {
+                        // Top 'R' robot wins
+                        healths[top_id] -= 1;
+                        healths[id] = 0; // Current robot is destroyed
+                    } else {
+                        // Tie: both are destroyed
+                        healths[id] = 0;
+                        healths[top_id] = 0;
+                        st.pop_back();
+                    }
                 }
-                else if(robots[j][1] > robots[i][1]) {
-                    robots[j][1]--;
-                    robots[i][1] = 0;
+                
+                // If the current 'L' robot survived all collisions, it joins the survivors
+                if (healths[id] > 0) {
+                    st.push_back(id);
                 }
-                else {
-                    robots[j][1] = 0;
-                    robots[i][1] = 0;
-                    st.pop();
-                }
-            }
-        }
-
-        vector<pair<int,int>> survivors;
-
-        for(auto &r : robots) {
-            if(r[1] > 0) {
-                survivors.push_back({r[3], r[1]});
             }
         }
-
-        sort(survivors.begin(), survivors.end());
-
-        vector<int> ans;
-
-        for(auto &[idx, hp] : survivors) {
-            ans.push_back(hp);
+        
+        // Collect surviving robots in their original order
+        vector<int> result;
+        for (int i = 0; i < n; ++i) {
+            if (healths[i] > 0) {
+                result.push_back(healths[i]);
+            }
         }
-
-        return ans;
+        
+        return result;
     }
 };
