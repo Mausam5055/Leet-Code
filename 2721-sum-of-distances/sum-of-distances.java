e325a581d0577bad9a8a52d16f9cd15d64f2cd60
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-09-04 17:04:04 +0530

    Time: 23 ms (75.64%) | Memory: 138.5 MB (26.23%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -1,44 +1,37 @@
-import java.util.*;
-
 class Solution {
     public long[] distance(int[] nums) {
         int n = nums.length;
-        long[] res = new long[n];
-
-        // Step 1: group indices by value
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, List<Integer>> indicesMap = new HashMap<>();
+        
         for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            indicesMap.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
         }
-
-        // Step 2: process each group
-        for (List<Integer> list : map.values()) {
-            int size = list.size();
-
-            // prefix sum array
-            long[] prefix = new long[size];
-            prefix[0] = list.get(0);
-            for (int i = 1; i < size; i++) {
-                prefix[i] = prefix[i - 1] + list.get(i);
+        
+        long[] ans = new long[n];
+        
+        for (List<Integer> indices : indicesMap.values()) {
+            int k = indices.size();
+            if (k == 1) continue;
+            
+            long prefixSum = 0;
+            long suffixSum = 0;
+            
+            for (int idx : indices) {
+                suffixSum += idx;
             }
-
-            for (int i = 0; i < size; i++) {
-                int idx = list.get(i);
-
-                // left
-                long leftSum = (i > 0) ? prefix[i - 1] : 0;
-                long leftCount = i;
-                long left = (long) idx * leftCount - leftSum;
-
-                // right
-                long rightSum = prefix[size - 1] - prefix[i];
-                long rightCount = size - i - 1;
-                long right = rightSum - (long) idx * rightCount;
-
-                res[idx] = left + right;
+            
+            for (int m = 0; m < k; m++) {
+                long i = indices.get(m);
+                suffixSum -= i;
+                
+                long leftDiff = m * i - prefixSum;
+                long rightDiff = suffixSum - (k - 1 - m) * i;
+                
+                ans[(int)i] = leftDiff + rightDiff;
+                prefixSum += i;
             }
         }
-
-        return res;
+        
+        return ans;
     }
 }
