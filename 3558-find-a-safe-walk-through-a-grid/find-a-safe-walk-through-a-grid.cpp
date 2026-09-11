42cf520666cfa65a2d2930c26de34db1427bbf19
Mausam Kar <104557109+Mausam5055@users.noreply.github.com>
2026-09-12 00:54:10 +0530

    Time: 11 ms (76.40%) | Memory: 32.8 MB (42.74%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        deque<pair<int,int>> dq;
+        const int INF = 1e9;
 
+        vector<vector<int>> dist(m, vector<int>(n, INF));
+
+        deque<pair<int, int>> dq;
+
+        // Starting cell also affects health
         dist[0][0] = grid[0][0];
         dq.push_front({0, 0});
 
@@ -20,15 +25,17 @@ public:
                 int nr = r + dr[k];
                 int nc = c + dc[k];
 
+                // Check bounds
                 if (nr < 0 || nr >= m || nc < 0 || nc >= n)
                     continue;
 
-                int w = grid[nr][nc];
+                int cost = grid[nr][nc];
 
-                if (dist[r][c] + w < dist[nr][nc]) {
-                    dist[nr][nc] = dist[r][c] + w;
+                // New health loss
+                if (dist[r][c] + cost < dist[nr][nc]) {
+                    dist[nr][nc] = dist[r][c] + cost;
 
-                    if (w == 0)
+                    if (cost == 0)
                         dq.push_front({nr, nc});
                     else
                         dq.push_back({nr, nc});
@@ -36,6 +43,7 @@ public:
             }
         }
 
-        return dist[m - 1][n - 1] < health;
+        // We must still have at least 1 health
+        return health - dist[m - 1][n - 1] >= 1;
     }
 };
